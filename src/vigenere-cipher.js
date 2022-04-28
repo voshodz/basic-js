926c5d9cf48578e464e928e74ab3a1d1cf1d6bbb
go2ramen <borcektrash@mail.ru>
2022-04-28 23:53:48 +0600

    feat: vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor(isDirect = true) {
+      if( (isDirect === true) || (isDirect === false) ) {
+        this.directMachine = isDirect;
+      } else {
+        throw new Error("Incorrect arguments!");
+      }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  charCode = (ch) => {
+    return ch.charCodeAt(0)-65
+  }
+  encrypt = (message, key) => {
+      if( typeof(message) !== 'string' || typeof(key) !== 'string'  ) { throw new Error("Incorrect arguments!");}
+      let result = "";
+      let upMsg = message.toUpperCase();
+      let upKey = key.toUpperCase();
+      let indexOfKey = 0;
+      for (let i = 0; i < upMsg.length; i++) {
+        if( (this.charCode(upMsg[i]) >=0) && (this.charCode(upMsg[i]) <= 25)) {
+            if(indexOfKey>upKey.length-1) {indexOfKey = 0;}
+            result += String.fromCharCode
+                    ( (this.charCode(upMsg[i]) + this.charCode(upKey[indexOfKey]))
+                      % 26 + 65
+                    );     
+            indexOfKey++;
+        } else {
+          result += upMsg[i];
+        }         
+      }
+      if(this.directMachine === true) {
+        return result;
+      } else {
+        return result.split('').reverse().join('');
+      }
+    }
+  decrypt = (message, key) => {
+      if( typeof(message) !== 'string' || typeof(key) !== 'string' ) {throw new Error("Incorrect arguments!");}
+      let result = "";
+      let upMsg = message.toUpperCase();
+      let upKey = key.toUpperCase();
+      let indexOfKey = 0;
+      for (let i = 0; i < upMsg.length; i++) {
+        if( (this.charCode(upMsg[i]) >=0) && (this.charCode(upMsg[i]) <= 25)) {
+            if(indexOfKey>upKey.length-1) {indexOfKey = 0;}
+            result += String.fromCharCode
+                    (( this.charCode(upMsg[i]) - this.charCode(upKey[indexOfKey]) + 26)
+                      % 26 + 65
+                    );     
+            indexOfKey++;
+        } else {
+          result += upMsg[i];
+        }         
+      }
+      if(this.directMachine === true) {
+        return result;
+      } else {
+        return result.split('').reverse().join('');
+      }
   }
 }
 
