94ea586caeebf750aee6133158bd5b5be15184f5
go2ramen <borcektrash@mail.ru>
2022-05-03 00:33:05 +0600

    feat: carbon-dation.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
-
+const koef = 0.693;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
@@ -17,10 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ function dateSample( sampleActivity) {
+      numb = Number(sampleActivity);
+      if( (typeof(sampleActivity) !== 'string')
+      || (Number.isNaN(numb)) ) {
+        return false;
+      }
+      if( (numb <= 0) || (numb > 15 ) ) {
+        return false;
+      }
+      let result;
+      result = (Math.log(MODERN_ACTIVITY / numb));
+      result = result / (koef / HALF_LIFE_PERIOD);
+      return Math.ceil(result);
+  } 
+
 
 module.exports = {
   dateSample
